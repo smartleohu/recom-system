b7f61226caf6787e12f094a4fa11780f6db12424
ruijinghu <oswasblood@msn.com>
2024-04-23 11:29:13 +0200

    update

--- recom_system/asgi.py
+++ recom_system/asgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recom_system.app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recom_system.settings')
 django_asgi_app = get_asgi_application()
 
 
--- recom_system/settings.py
+++ recom_system/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8080',
+    'http://127.0.0.1:8000',
 ]
 
 ROOT_URLCONF = 'recom_system.app.urls'
