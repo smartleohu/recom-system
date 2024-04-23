3e93a0be763caa05fb695aef67741295d24784bd
ruijinghu <oswasblood@msn.com>
2024-04-23 10:58:05 +0200

    update

--- tests/test_app_views.py
+++ tests/test_app_views.py
@@ -24,15 +24,40 @@ def test_calculate_similar_anomalies(mock_delay, sample_request):
                              'user_name': 'test_user'}
 
 
+from unittest.mock import MagicMock, patch
+
+import pytest
+from celery.result import AsyncResult
+
+from recom_system.app.views import fetch_similar_anomalies
+
+
+@pytest.mark.django_db
+@patch('recom_system.app.views.AsyncResult')
 @patch('recom_system.app.views.ElasticSearchStorage.get_instance')
-def test_fetch_similar_anomalies(mock_get_instance, sample_request):
+def test_fetch_similar_anomalies(mock_get_instance, mock_AsyncResult,
+                                 sample_request):
+    # Mocking ElasticSearchStorage
     mock_instance = MagicMock()
     mock_instance.get_similarities_by_res_id.return_value = {
         'result': 'mock_result'}
     mock_get_instance.return_value = mock_instance
+
+    # Mocking AsyncResult
+    async_result_instance = MagicMock()
+    async_result_instance.status = 'SUCCESS'
+    async_result_instance.result = {
+        'search_results': {'result': 'mock_result'}}
+    mock_AsyncResult.return_value = async_result_instance
+
+    # Simulate request
     sample_request.method = 'GET'
     sample_request.query_params = {'task_result_id': 'test_task_result_id',
                                    'user_name': 'test_user'}
+
+    # Call the view function
     response = fetch_similar_anomalies(sample_request)
+
+    # Assertions
     assert response.status_code == 200
     assert response.data == {'search_results': {'result': 'mock_result'}}
